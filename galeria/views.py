df5d21e9ea224d40586b3126d8c958e572c21134
Leonardo <contato.leonardoap@gmail.com>
2025-04-29 20:02:45 -0300

    instalando banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1:{'nome': 'Nebulosa de Carina',
+       'legenda' : 'WebbTelescope / Nasa / james Webb'},
+    2:{'nome': 'galax de Carina',
+       'legenda' : 'Telescope / Nasa / james Webb'},
+}
+    return render(request, 'galeria/index.html', {'cards':dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
