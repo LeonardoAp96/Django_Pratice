b853bd6a81d488ec1aee5f15b0374f54440acd76
Leonardo <contato.leonardoap@gmail.com>
2025-05-03 17:23:57 -0300

    geranciando os arquivos pelo admi do django

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-
+from galeria.models import Fotografia
 
 def index(request):
     dados = {
@@ -8,8 +8,11 @@ def index(request):
        'legenda' : 'WebbTelescope / Nasa / james Webb'},
     2:{'nome': 'galax de Carina',
        'legenda' : 'Telescope / Nasa / james Webb'},
-}
-    return render(request, 'galeria/index.html', {'cards':dados})
+    }
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards':fotografias})
+
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
