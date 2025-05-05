5dab14b9a99a3ec8d11295133c95daf01a09882d
Leonardo <contato.leonardoap@gmail.com>
2025-05-05 18:03:56 -0300

    sistema de busca de imagem

--- galeria/views.py
+++ galeria/views.py
@@ -10,9 +10,17 @@ def index(request):
        'legenda' : 'Telescope / Nasa / james Webb'},
     }
 
-    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request, 'galeria/index.html', {'cards':fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+   fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+   if  'buscar' in request.GET:
+       nome_buscar = request.GET['buscar']
+       if nome_buscar:
+           fotografias = fotografias.filter(nome__icontains=nome_buscar)
+   return render(request, 'galeria/buscar.html', {'cards': fotografias})
